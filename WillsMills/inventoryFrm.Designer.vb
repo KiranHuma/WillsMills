9ea5ddbdbe642002b3fa40cf36e1b8585290912f
Aimal Huma <66478525+KiranHuma@users.noreply.github.com>
2022-11-10 10:55:17 +0500

    invetory_to_textbox

--- WillsMills/inventoryFrm.Designer.vb
+++ WillsMills/inventoryFrm.Designer.vb
@@ -25,7 +25,6 @@ Partial Class inventoryFrm
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(inventoryFrm))
         Me.productPhoto_picture = New System.Windows.Forms.PictureBox()
         Me.prodctCode_txt = New System.Windows.Forms.TextBox()
-        Me.prodctName_txt = New System.Windows.Forms.TextBox()
         Me.originalPrice_txt = New System.Windows.Forms.TextBox()
         Me.resellerRate_txt = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
@@ -57,6 +56,17 @@ Partial Class inventoryFrm
         Me.Label12 = New System.Windows.Forms.Label()
         Me.inventory_Grid = New System.Windows.Forms.DataGridView()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.productSize_txt = New System.Windows.Forms.ComboBox()
+        Me.productColor_txt = New System.Windows.Forms.ComboBox()
+        Me.productSeason_txt = New System.Windows.Forms.ComboBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.prodctName_txt = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.productCategory_txt = New System.Windows.Forms.ComboBox()
+        Me.productStatus_txt = New System.Windows.Forms.ComboBox()
+        Me.Label17 = New System.Windows.Forms.Label()
         CType(Me.productPhoto_picture, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.inventory_Grid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
@@ -74,28 +84,21 @@ Partial Class inventoryFrm
         '
         'prodctCode_txt
         '
-        Me.prodctCode_txt.Location = New System.Drawing.Point(203, 168)
+        Me.prodctCode_txt.Location = New System.Drawing.Point(203, 127)
         Me.prodctCode_txt.Name = "prodctCode_txt"
         Me.prodctCode_txt.Size = New System.Drawing.Size(267, 22)
         Me.prodctCode_txt.TabIndex = 2
         '
-        'prodctName_txt
-        '
-        Me.prodctName_txt.Location = New System.Drawing.Point(203, 207)
-        Me.prodctName_txt.Name = "prodctName_txt"
-        Me.prodctName_txt.Size = New System.Drawing.Size(267, 22)
-        Me.prodctName_txt.TabIndex = 3
-        '
         'originalPrice_txt
         '
-        Me.originalPrice_txt.Location = New System.Drawing.Point(202, 245)
+        Me.originalPrice_txt.Location = New System.Drawing.Point(205, 319)
         Me.originalPrice_txt.Name = "originalPrice_txt"
         Me.originalPrice_txt.Size = New System.Drawing.Size(267, 22)
         Me.originalPrice_txt.TabIndex = 4
         '
         'resellerRate_txt
         '
-        Me.resellerRate_txt.Location = New System.Drawing.Point(202, 288)
+        Me.resellerRate_txt.Location = New System.Drawing.Point(205, 362)
         Me.resellerRate_txt.Name = "resellerRate_txt"
         Me.resellerRate_txt.Size = New System.Drawing.Size(267, 22)
         Me.resellerRate_txt.TabIndex = 5
@@ -104,7 +107,7 @@ Partial Class inventoryFrm
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(99, 133)
+        Me.Label1.Location = New System.Drawing.Point(98, 104)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(84, 17)
         Me.Label1.TabIndex = 6
@@ -113,7 +116,7 @@ Partial Class inventoryFrm
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(99, 173)
+        Me.Label2.Location = New System.Drawing.Point(99, 132)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(94, 17)
         Me.Label2.TabIndex = 7
@@ -122,16 +125,16 @@ Partial Class inventoryFrm
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(98, 207)
+        Me.Label3.Location = New System.Drawing.Point(99, 195)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(98, 17)
+        Me.Label3.Size = New System.Drawing.Size(41, 17)
         Me.Label3.TabIndex = 8
-        Me.Label3.Text = "Product Name"
+        Me.Label3.Text = "Color"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(98, 245)
+        Me.Label5.Location = New System.Drawing.Point(101, 319)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(93, 17)
         Me.Label5.TabIndex = 9
@@ -140,7 +143,7 @@ Partial Class inventoryFrm
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(124, 396)
+        Me.Label6.Location = New System.Drawing.Point(127, 470)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(67, 17)
         Me.Label6.TabIndex = 10
@@ -159,7 +162,7 @@ Partial Class inventoryFrm
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(100, 419)
+        Me.Label7.Location = New System.Drawing.Point(103, 493)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(79, 17)
         Me.Label7.TabIndex = 12
@@ -167,16 +170,16 @@ Partial Class inventoryFrm
         '
         'description_txt
         '
-        Me.description_txt.Location = New System.Drawing.Point(204, 419)
+        Me.description_txt.Location = New System.Drawing.Point(207, 493)
         Me.description_txt.Name = "description_txt"
-        Me.description_txt.Size = New System.Drawing.Size(267, 96)
+        Me.description_txt.Size = New System.Drawing.Size(470, 169)
         Me.description_txt.TabIndex = 13
         Me.description_txt.Text = ""
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(56, 288)
+        Me.Label8.Location = New System.Drawing.Point(59, 362)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(137, 17)
         Me.Label8.TabIndex = 14
@@ -184,7 +187,7 @@ Partial Class inventoryFrm
         '
         'sellRate_txt
         '
-        Me.sellRate_txt.Location = New System.Drawing.Point(204, 393)
+        Me.sellRate_txt.Location = New System.Drawing.Point(207, 467)
         Me.sellRate_txt.Name = "sellRate_txt"
         Me.sellRate_txt.Size = New System.Drawing.Size(267, 22)
         Me.sellRate_txt.TabIndex = 15
@@ -192,7 +195,7 @@ Partial Class inventoryFrm
         'wholeSeller_txt
         '
         Me.wholeSeller_txt.FormattingEnabled = True
-        Me.wholeSeller_txt.Location = New System.Drawing.Point(203, 126)
+        Me.wholeSeller_txt.Location = New System.Drawing.Point(202, 97)
         Me.wholeSeller_txt.Name = "wholeSeller_txt"
         Me.wholeSeller_txt.Size = New System.Drawing.Size(268, 24)
         Me.wholeSeller_txt.TabIndex = 16
@@ -221,7 +224,7 @@ Partial Class inventoryFrm
         '
         'btnSave
         '
-        Me.btnSave.Location = New System.Drawing.Point(396, 541)
+        Me.btnSave.Location = New System.Drawing.Point(602, 690)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(75, 28)
         Me.btnSave.TabIndex = 19
@@ -249,7 +252,7 @@ Partial Class inventoryFrm
         'statusMsg_lbl
         '
         Me.statusMsg_lbl.AutoSize = True
-        Me.statusMsg_lbl.Location = New System.Drawing.Point(12, 521)
+        Me.statusMsg_lbl.Location = New System.Drawing.Point(744, 668)
         Me.statusMsg_lbl.Name = "statusMsg_lbl"
         Me.statusMsg_lbl.Size = New System.Drawing.Size(59, 17)
         Me.statusMsg_lbl.TabIndex = 22
@@ -273,7 +276,7 @@ Partial Class inventoryFrm
         '
         'productID_txt
         '
-        Me.productID_txt.Location = New System.Drawing.Point(204, 98)
+        Me.productID_txt.Location = New System.Drawing.Point(203, 69)
         Me.productID_txt.Name = "productID_txt"
         Me.productID_txt.Size = New System.Drawing.Size(267, 22)
         Me.productID_txt.TabIndex = 27
@@ -291,7 +294,7 @@ Partial Class inventoryFrm
         '
         'Asset_Code_txt
         '
-        Me.Asset_Code_txt.Location = New System.Drawing.Point(15, 22)
+        Me.Asset_Code_txt.Location = New System.Drawing.Point(281, 14)
         Me.Asset_Code_txt.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Asset_Code_txt.Name = "Asset_Code_txt"
         Me.Asset_Code_txt.Size = New System.Drawing.Size(29, 22)
@@ -301,7 +304,7 @@ Partial Class inventoryFrm
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(99, 332)
+        Me.Label11.Location = New System.Drawing.Point(102, 406)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(93, 17)
         Me.Label11.TabIndex = 48
@@ -309,7 +312,7 @@ Partial Class inventoryFrm
         '
         'couponPrice_txt
         '
-        Me.couponPrice_txt.Location = New System.Drawing.Point(204, 327)
+        Me.couponPrice_txt.Location = New System.Drawing.Point(207, 401)
         Me.couponPrice_txt.Name = "couponPrice_txt"
         Me.couponPrice_txt.Size = New System.Drawing.Size(267, 22)
         Me.couponPrice_txt.TabIndex = 47
@@ -317,7 +320,7 @@ Partial Class inventoryFrm
         '
         'willMillsPrice_txt
         '
-        Me.willMillsPrice_txt.Location = New System.Drawing.Point(204, 367)
+        Me.willMillsPrice_txt.Location = New System.Drawing.Point(207, 441)
         Me.willMillsPrice_txt.Name = "willMillsPrice_txt"
         Me.willMillsPrice_txt.Size = New System.Drawing.Size(267, 22)
         Me.willMillsPrice_txt.TabIndex = 50
@@ -325,7 +328,7 @@ Partial Class inventoryFrm
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(91, 370)
+        Me.Label12.Location = New System.Drawing.Point(94, 444)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(100, 17)
         Me.Label12.TabIndex = 49
@@ -355,11 +358,115 @@ Partial Class inventoryFrm
         Me.Panel1.Size = New System.Drawing.Size(43, 21)
         Me.Panel1.TabIndex = 54
         '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(99, 223)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(35, 17)
+        Me.Label13.TabIndex = 56
+        Me.Label13.Text = "Size"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(99, 251)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(65, 17)
+        Me.Label14.TabIndex = 58
+        Me.Label14.Text = "Category"
+        '
+        'productSize_txt
+        '
+        Me.productSize_txt.FormattingEnabled = True
+        Me.productSize_txt.Location = New System.Drawing.Point(204, 218)
+        Me.productSize_txt.Name = "productSize_txt"
+        Me.productSize_txt.Size = New System.Drawing.Size(268, 24)
+        Me.productSize_txt.TabIndex = 59
+        '
+        'productColor_txt
+        '
+        Me.productColor_txt.FormattingEnabled = True
+        Me.productColor_txt.Location = New System.Drawing.Point(204, 188)
+        Me.productColor_txt.Name = "productColor_txt"
+        Me.productColor_txt.Size = New System.Drawing.Size(268, 24)
+        Me.productColor_txt.TabIndex = 60
+        '
+        'productSeason_txt
+        '
+        Me.productSeason_txt.FormattingEnabled = True
+        Me.productSeason_txt.Location = New System.Drawing.Point(205, 279)
+        Me.productSeason_txt.Name = "productSeason_txt"
+        Me.productSeason_txt.Size = New System.Drawing.Size(268, 24)
+        Me.productSeason_txt.TabIndex = 64
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(97, 160)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(98, 17)
+        Me.Label15.TabIndex = 63
+        Me.Label15.Text = "Product Name"
+        '
+        'prodctName_txt
+        '
+        Me.prodctName_txt.Location = New System.Drawing.Point(206, 160)
+        Me.prodctName_txt.Name = "prodctName_txt"
+        Me.prodctName_txt.Size = New System.Drawing.Size(267, 22)
+        Me.prodctName_txt.TabIndex = 62
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(100, 284)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(56, 17)
+        Me.Label16.TabIndex = 61
+        Me.Label16.Text = "Season"
+        '
+        'productCategory_txt
+        '
+        Me.productCategory_txt.FormattingEnabled = True
+        Me.productCategory_txt.Location = New System.Drawing.Point(203, 248)
+        Me.productCategory_txt.Name = "productCategory_txt"
+        Me.productCategory_txt.Size = New System.Drawing.Size(268, 24)
+        Me.productCategory_txt.TabIndex = 65
+        '
+        'productStatus_txt
+        '
+        Me.productStatus_txt.FormattingEnabled = True
+        Me.productStatus_txt.Items.AddRange(New Object() {"Pending", "Uploaded"})
+        Me.productStatus_txt.Location = New System.Drawing.Point(396, 694)
+        Me.productStatus_txt.Name = "productStatus_txt"
+        Me.productStatus_txt.Size = New System.Drawing.Size(137, 24)
+        Me.productStatus_txt.TabIndex = 67
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(400, 674)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(133, 17)
+        Me.Label17.TabIndex = 66
+        Me.Label17.Text = "Upload on WillsMills"
+        '
         'inventoryFrm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1437, 630)
+        Me.ClientSize = New System.Drawing.Size(1437, 772)
+        Me.Controls.Add(Me.productStatus_txt)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.productCategory_txt)
+        Me.Controls.Add(Me.productSeason_txt)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.prodctName_txt)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.productColor_txt)
+        Me.Controls.Add(Me.productSize_txt)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label13)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.inventory_Grid)
         Me.Controls.Add(Me.willMillsPrice_txt)
@@ -386,7 +493,6 @@ Partial Class inventoryFrm
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.resellerRate_txt)
         Me.Controls.Add(Me.originalPrice_txt)
-        Me.Controls.Add(Me.prodctName_txt)
         Me.Controls.Add(Me.prodctCode_txt)
         Me.Controls.Add(Me.productPhoto_picture)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -403,7 +509,6 @@ Partial Class inventoryFrm
     End Sub
     Friend WithEvents productPhoto_picture As PictureBox
     Friend WithEvents prodctCode_txt As TextBox
-    Friend WithEvents prodctName_txt As TextBox
     Friend WithEvents originalPrice_txt As TextBox
     Friend WithEvents resellerRate_txt As TextBox
     Friend WithEvents Label1 As Label
@@ -435,4 +540,15 @@ Partial Class inventoryFrm
     Friend WithEvents Label12 As Label
     Friend WithEvents inventory_Grid As DataGridView
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents productSize_txt As ComboBox
+    Friend WithEvents productColor_txt As ComboBox
+    Friend WithEvents productSeason_txt As ComboBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents prodctName_txt As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents productCategory_txt As ComboBox
+    Friend WithEvents productStatus_txt As ComboBox
+    Friend WithEvents Label17 As Label
 End Class
